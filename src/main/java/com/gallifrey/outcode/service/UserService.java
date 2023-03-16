2fa58b132e21b4e95b4e8592d832ba200080d9d4
gallifey <956465331@qq.com>
2023-03-17 02:10:21 +0800

    Kaptcha(Captcha) Ticket身份验证(Cookie+mysql实现) login function logout function

--- src/main/java/com/gallifrey/outcode/service/UserService.java
+++ src/main/java/com/gallifrey/outcode/service/UserService.java
@@ -1,6 +1,8 @@
 package com.gallifrey.outcode.service;
 
+import com.gallifrey.outcode.dao.LoginTicketMapper;
 import com.gallifrey.outcode.dao.UserMapper;
+import com.gallifrey.outcode.entity.LoginTicket;
 import com.gallifrey.outcode.entity.User;
 import com.gallifrey.outcode.util.CommunityConstant;
 import com.gallifrey.outcode.util.CommunityUtil;
@@ -12,8 +14,6 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
-
-import java.sql.Struct;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,10 +25,12 @@ public class UserService implements CommunityConstant {
     private UserMapper userMapper;
     private MailClient mailClient;
     private TemplateEngine templateEngine;
+
     @Autowired
     public void setMailClient(MailClient mailClient) {
         this.mailClient = mailClient;
     }
+
     @Autowired
     public void setTemplateEngine(TemplateEngine templateEngine) {
         this.templateEngine = templateEngine;
@@ -49,80 +51,125 @@ public class UserService implements CommunityConstant {
         return userMapper.selectById(id);
     }
 
-    public Map<String,Object> register(User user){
-        Map<String,Object> map=new HashMap<>();
+    public Map<String, Object> register(User user) {
+        Map<String, Object> map = new HashMap<>();
         //空值处理
-        if(user==null){
+        if (user == null) {
             throw new IllegalArgumentException("参数不能为空");
         }
 
-        if(StringUtils.isBlank(user.getUsername())){
-            map.put("usernameMsg","账号不能为空");
+        if (StringUtils.isBlank(user.getUsername())) {
+            map.put("usernameMsg", "账号不能为空");
             return map;
         }
 
-        if(StringUtils.isBlank(user.getPassword())){
-            map.put("passwordMsg","密码不能为空");
+        if (StringUtils.isBlank(user.getPassword())) {
+            map.put("passwordMsg", "密码不能为空");
             return map;
         }
 
-        if(StringUtils.isBlank(user.getEmail())){
-            map.put("emailMsg","邮箱不能为空");
+        if (StringUtils.isBlank(user.getEmail())) {
+            map.put("emailMsg", "邮箱不能为空");
             return map;
         }
 
         //验证账号
-        User u=userMapper.selectByName(user.getUsername());
-        if(u!=null){
-            map.put("usernameMsg","账号已存在");
+        User u = userMapper.selectByName(user.getUsername());
+        if (u != null) {
+            map.put("usernameMsg", "账号已存在");
             return map;
         }
         //验证邮箱
-        u=userMapper.selectByEmail(user.getEmail());
-        if(u!=null){
-            map.put("emailMsg","邮箱已注册");
+        u = userMapper.selectByEmail(user.getEmail());
+        if (u != null) {
+            map.put("emailMsg", "邮箱已注册");
             return map;
         }
 
 
         //注册用户
-        user.setSalt(CommunityUtil.generateUUID().substring(0,5));
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
 
-        user.setPassword(CommunityUtil.md5(user.getPassword())+user.getSalt());
+        user.setPassword(CommunityUtil.md5(user.getPassword() + user.getSalt()));
         user.setType(0);
         user.setStatus(0);
         user.setActivationCode(CommunityUtil.generateUUID());
-        user.setHeaderUrl(String.format("https://images.nowcoder.com/head/%dt.png",new Random().nextInt(1000)));
+        user.setHeaderUrl(String.format("https://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000)));
         user.setCreateTime(new Date());
         userMapper.insertUser(user);
 
         //激活邮件
-        Context context=new Context();
-        context.setVariable("email",user.getEmail());
+        Context context = new Context();
+        context.setVariable("email", user.getEmail());
         //http://localhost:9079/community/activation/101/code
-        String url=domain+contextPath+"/activation/"+user.getId()+"/"+user.getActivationCode();
-        context.setVariable("url",url);
-        String content=templateEngine.process("/mail/activation",context);
-        mailClient.sendMail(user.getEmail(),"激活账号",content);
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        context.setVariable("url", url);
+        String content = templateEngine.process("/mail/activation", context);
+        mailClient.sendMail(user.getEmail(), "激活账号", content);
 
         return map;
     }
 
 
-
-    public int activation(int userId,String code){
-        User user=userMapper.selectById(userId);
-        if(user.getStatus()==1){
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if (user.getStatus() == 1) {
             return ACTIVATION_REPEAT;
-        }else if(user.getActivationCode().equals(code)){
-            userMapper.updateStatus(userId,1);
+        } else if (user.getActivationCode().equals(code)) {
+            userMapper.updateStatus(userId, 1);
             return ACTIVATION_SUCCESS;
-        }else {
+        } else {
             return ACTIVATION_FAILURE;
         }
     }
 
+    private LoginTicketMapper loginTicketMapper;
 
+    @Autowired
+    public void setLoginTicketMapper(LoginTicketMapper loginTicketMapper) {
+        this.loginTicketMapper = loginTicketMapper;
+    }
 
+    public Map<String, Object> login(String username, String password, int expiredSeconds) {
+        Map<String, Object> map = new HashMap<>();
+        //空值处理
+        if (StringUtils.isBlank(username)) {
+            map.put("usernameMsg", "账号不能为空");
+            return map;
+        }
+        if (StringUtils.isBlank(password)) {
+            map.put("passwordMsg", "密码不能为空");
+            return map;
+        }
+        //验证账号
+        User user = userMapper.selectByName(username);
+        if (user == null) {
+            map.put("usernameMsg", "账号不存在");
+            return map;
+        }
+        //验证状态
+        if (user.getStatus() == 0) {
+            map.put("usernameMsg", "该账号未激活");
+            return map;
+        }
+        //验证密码
+        password = CommunityUtil.md5(password + user.getSalt());
+        if (!user.getPassword().equals(password)) {
+            map.put("passwordMsg", "密码不正确");
+            return map;
+        }
+        //生成登录凭证
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(CommunityUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSeconds * 1000L));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+        map.put("ticket", loginTicket.getTicket());
+        return map;
+    }
 
+    public void logout(String ticket) {
+        loginTicketMapper.updateStatus(ticket, 1);
+    }
 }
--- src/test/java/com/gallifrey/outcode/MapperTests.java
+++ src/test/java/com/gallifrey/outcode/MapperTests.java
@@ -1,8 +1,9 @@
 package com.gallifrey.outcode;
 
 import com.gallifrey.outcode.dao.DiscussPostMapper;
+import com.gallifrey.outcode.dao.LoginTicketMapper;
 import com.gallifrey.outcode.entity.DiscussPost;
-import org.junit.runner.RunWith;
+import com.gallifrey.outcode.entity.LoginTicket;
 import com.gallifrey.outcode.dao.UserMapper;
 import com.gallifrey.outcode.entity.User;
 
@@ -22,6 +23,8 @@ public class MapperTests {
     private UserMapper userMapper;
     @Autowired
     private DiscussPostMapper discussPostMapper;
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     @Test
     public void testSelectUser() {
@@ -67,6 +70,28 @@ public class MapperTests {
 
         int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
+    }
+
+
+    @Test
+    public void testInsertLoginTicket() {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setTicket("abd");
+        loginTicket.setId(101);
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket() {
+
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abd");
+        System.out.println(loginTicket);
+        loginTicketMapper.updateStatus("abd", 1);
+        loginTicket = loginTicketMapper.selectByTicket("abd");
+        System.out.println(loginTicket);
+
     }
 
 }
