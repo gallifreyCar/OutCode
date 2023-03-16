993e6f8533b74082f355a134ad2d2e17ed1a97f9
gallifey <956465331@qq.com>
2023-03-16 10:53:29 +0800

    Cookie && Session 分布式场景下Session带来的问题与解决方案

--- src/main/java/com/gallifrey/outcode/controller/AlphaController.java
+++ src/main/java/com/gallifrey/outcode/controller/AlphaController.java
@@ -1,22 +1,24 @@
 package com.gallifrey.outcode.controller;
 
-import com.gallifrey.outcode.dao.AlphaDao;
-import com.gallifrey.outcode.dao.AlphaDaoMybatisImpl;
+
 import com.gallifrey.outcode.service.AlphaService;
+import com.gallifrey.outcode.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 
 @Controller
-@RequestMapping("/Alpha")
+@RequestMapping("/alpha")
 public class AlphaController {
 
     private final AlphaService alphaService;
@@ -56,7 +58,7 @@ public class AlphaController {
         // 返回响应数据
         response.setContentType("text/html;charset=utf-8");
 
-        try (PrintWriter writer = response.getWriter();) {
+        try (PrintWriter writer = response.getWriter()) {
             writer.write("<h1>OutCode网<h1>");
 
         } catch (IOException e) {
@@ -149,5 +151,44 @@ public class AlphaController {
         list.add(emp2);
         list.add(emp3);
         return list;
+    }
+
+    //cookie示例
+    @RequestMapping(path = "/cookie/set",method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response){
+        //  创建cookie
+        Cookie cookie=new Cookie("code", CommunityUtil.generateUUID());
+        //  设置cookie生效的范围
+        cookie.setPath("/community/alpha");
+        //  设置cookie的生存时间
+        cookie.setMaxAge(60*10);
+        //  发送cookie
+        response.addCookie(cookie);
+        return "set cookie";
+    }
+
+    @RequestMapping(path = "cookie/get",method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code){
+        System.out.println(code);
+        return "get cookie";
+    }
+
+
+    //session示例
+    @RequestMapping(path = "session/set",method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session){
+        session.setAttribute("id",1);
+        session.setAttribute("name","Test");
+        return "set session";
+    }
+    @RequestMapping(path = "session/get",method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session){
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "get session";
     }
 }
